ec662127f225fdf039c220df174ad6c12ce83de0
F1XI <fixihartmann@yahoo.de>
2022-06-12 20:40:08 +0200

    Add command to build jar

---
+++ TankstellePreisabfrage/build.bat
@@ -0,0 +1,3 @@
+::Uses maven from https://dlcdn.apache.org/maven/maven-3/3.8.5/binaries/apache-maven-3.8.5-bin.zip
+::mavens /bin directory MUST added to the Environment PATH variable
+mvn clean install
